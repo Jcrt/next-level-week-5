497b8114a418b0446c79c43f8985e5b1ce4efb35
Julio Trindade <julio.rosatrindade@gmail.com>
2021-04-25 00:11:16 -0300

    Inicio da aula 4

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -47,7 +47,13 @@ export default function Episode({episode} : EpisodeProps){
 
 export const getStaticPaths: GetStaticPaths = async() => {
   return {
-    paths: [], 
+    paths: [
+      {
+        params: { 
+          slug: 'a-importancia-da-contribuicao-em-open-source'
+        }
+      }
+    ], 
     fallback: 'blocking'
   }
 }
